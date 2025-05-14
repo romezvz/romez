5fb1c5e1cdac7d66fdc9f66b400c51ec0f4efcd4
romerojoseing <romerojoseing@gmail.com>
2025-05-14 21:23:45 +0200

    feat(master): Create Project

---
+++ src/test.ts
@@ -0,0 +1,26 @@
+import { getTestBed } from '@angular/core/testing'
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing'
+import 'zone.js/testing'
+
+declare const require: {
+  context(
+    path: string,
+    deep?: boolean,
+    filter?: RegExp
+  ): {
+    keys(): string[]
+    <T>(id: string): T
+  }
+}
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+)
+
+// Busca todos los archivos .spec.ts recursivamente
+const context = require.context('./', true, /\.spec\.ts$/)
+context.keys().forEach(context)
