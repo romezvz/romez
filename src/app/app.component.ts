a5b0a0558577a9fbae0a798d46aa20f3b9baf48a
romerojoseing <romerojoseing@gmail.com>
2025-05-14 20:56:31 +0200

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
+  standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'romez';
+  title = 'romez'
 }
